9a1d699063b6cc9b6d85c538bcc65a74245d3b8c
baojame2 <baojame2@teach.cs.utoronto.ca>
2018-01-27 11:38:56 -0500

    basic info and stuff

---
+++ check avg of departments.py
@@ -0,0 +1,8 @@
+file1 = read("hackathon_dataset_2010.dat")
+file2 = read("hackathon_dataset_2011.dat")
+file3 = read("hackathon_dataset_2012.dat")
+
+a = file1.readlines()
+b = file2.readlines()
+c = file3.readlines()
+
